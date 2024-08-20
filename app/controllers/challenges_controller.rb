727f5cf6f7987665b0ac42edbe2543b3199ea331
Muriel Ighmouracene <muriel.ighmouracene@gmail.com>
2024-08-20 16:23:00 +0200

    Index+show+cards

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,4 +1,10 @@
 class ChallengesController < ApplicationController
-  
-  #index/show/new/create/edit/update
+  # index/show/new/create/edit/update
+  def index
+    @challenges = Challenge.all
+  end
+
+  def show
+    @challenge = Challenge.find(params[:id])
+  end
 end
