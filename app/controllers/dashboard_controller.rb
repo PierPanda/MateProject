2667c72acf3e5bb652cf685819315f3d55156ec8
Muriel Ighmouracene <muriel.ighmouracene@gmail.com>
2024-08-23 11:53:27 +0200

    Install Cloudinary

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,25 @@
+# class DashboardController < ApplicationController
+#   def index
+#     @user_challenges = current_user.user_challenges
+#     if params[:status] == 'in_progress'
+#     @user_challenges = @user_challenges.where(done: false)
+#     # elsif params[:status] == 'personal_challenges'
+#     #   @personal_challenges = user_challenges.personal_challenge
+#     else params[:status] == 'accomplished'
+#     @user_challenges = @user_challenges.where(done: true)
+#     end
+#   end
+# end
+
 class DashboardController < ApplicationController
   def index
     @user_challenges = current_user.user_challenges
-    if params[:status] == "in_progress"
+    if params[:status] == 'in_progress'
       @user_challenges = @user_challenges.where(done: false)
+    # elsif params[:status] == 'personal_challenges'
+    #   @personal_challenges = Challenge.where()
+    elsif params[:status] == 'accomplished'
+      @user_challenges = @user_challenges.where(done: true)
     end
   end
 end
