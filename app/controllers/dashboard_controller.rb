53ace9decab2a7eba05967fedc72213d5feac72e
Muriel Ighmouracene <muriel.ighmouracene@gmail.com>
2024-08-23 12:33:41 +0200

    Début set up Cloudinary

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -13,13 +13,17 @@
 
 class DashboardController < ApplicationController
   def index
-    @user_challenges = current_user.user_challenges
+    @user_challenges = current_user.user_challenges.where(done: false)
+    @in_progress = true
+    @accomplished = false
     if params[:status] == 'in_progress'
-      @user_challenges = @user_challenges.where(done: false)
+      @user_challenges
     # elsif params[:status] == 'personal_challenges'
     #   @personal_challenges = Challenge.where()
     elsif params[:status] == 'accomplished'
-      @user_challenges = @user_challenges.where(done: true)
+      @in_progress = false
+      @accomplished = true
+      @user_challenges = current_user.user_challenges.where(done: true)
     end
   end
 end
