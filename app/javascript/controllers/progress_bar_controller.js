03330063a19c3807227e8c2eeccf092cbdb07001
Anais ALLALI <anais.allali@kedgebs.com>
2024-08-29 17:54:39 +0200

    changement de la progressbar

--- app/javascript/controllers/progress_bar_controller.js
+++ app/javascript/controllers/progress_bar_controller.js
@@ -1,61 +1,24 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["circlePercent", "roundPer"];
+  static targets = ["fill", "percentage"];
   static values = {
     pourcentage: Number
   }
 
   connect() {
-    const dataV = this.pourcentageValue;
-    const dataDeg = dataV * 3.6;
-    const roundPer = this.roundPerTarget;
-
-    roundPer.style.transform = `rotate(${parseInt(dataDeg + 180)}deg)`;
-
-    const circleInbox = document.createElement("div");
-    circleInbox.className = "circle_inbox";
-    const percentText = document.createElement("span");
-    percentText.className = "percent_text";
-    circleInbox.appendChild(percentText);
-    this.circlePercentTarget.appendChild(circleInbox);
-
-    this.animateCounter(this.circlePercentTarget, percentText, dataV);
-
-    if (dataV >= 51) {
-      roundPer.style.transform = "rotate(360deg)";
-      setTimeout(() => {
-        this.circlePercentTarget.classList.add("percent_more");
-      }, 1000);
-      setTimeout(() => {
-        roundPer.style.transform = `rotate(${parseInt(dataDeg + 180)}deg)`;
-      }, 1000);
-    }
+    this.updateChart();
   }
 
-  animateCounter(element, textElement, value) {
-    let counter = { value: 0 };
-    const duration = 2000;
-    const easing = function(t) {
-      return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
-    };
+  updateChart() {
+    const percent = this.pourcentageValue;
+    const deg = 360 * percent / 100;
 
-    const startAnimation = (startTime) => {
-      const animateStep = (timestamp) => {
-        if (!startTime) startTime = timestamp;
-        const progress = timestamp - startTime;
-        const easingProgress = easing(progress / duration);
-        counter.value = easingProgress * value;
-        textElement.textContent = `${Math.ceil(counter.value)}%`;
+    if (percent > 50) {
+      this.element.classList.add('gt-50');
+    }
 
-        if (progress < duration) {
-          requestAnimationFrame(animateStep);
-        }
-      };
-
-      requestAnimationFrame(animateStep);
-    };
-
-    startAnimation();
+    this.fillTarget.style.transform = `rotate(${deg}deg)`;
+    this.percentageTarget.textContent = `${percent}%`;
   }
 }
